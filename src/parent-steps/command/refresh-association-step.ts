4f30539d3671d69242425c19b92cb3dcb2795522
Andrew Kennett <kenneans@amazon.com>
2022-06-03 13:06:10 -0700

    feat: Implement Refresh Association Plugin.

---
+++ src/parent-steps/command/refresh-association-step.ts
@@ -0,0 +1,49 @@
+import { Construct } from 'constructs';
+import { Platform } from '../../domain/platform';
+import { IStringListVariable } from '../../interface/variables/string-list-variable';
+import { CommandStep, CommandStepProps } from '../command-step';
+
+/**
+ * Properties refreshAssociation step
+ */
+export interface RefreshAssociationStepProps extends CommandStepProps {
+
+    /**
+     * List of association IDs. If empty, all associations bound to the specified target are applied.
+     */
+    readonly associationIds: IStringListVariable;
+
+}
+
+/**
+ * AutomationStep implemenation for aws:refreshAssociation
+ * https://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-plugins.html#aws-refreshassociation
+ */
+export class RefreshAssociationStep extends CommandStep {
+
+    readonly associationIds: IStringListVariable;
+
+    readonly platforms = [Platform.WINDOWS, Platform.LINUX];
+    readonly action = 'aws:refreshAssociation';
+
+    constructor(scope: Construct, id: string, props: RefreshAssociationStepProps) {
+        super(scope, id, props);
+        this.associationIds = props.associationIds;
+    }
+
+    /**
+     * Input required for this command is associationIds
+     */
+    public listInputs(): string[] {
+        const inputs = [
+            this.associationIds,
+        ]
+        return inputs.flatMap(i => i?.requiredInputs() ?? []);
+    }
+
+    public toSsmEntry(): { [name: string]: any } {
+        const inputs: { [name: string]: any } = { associationIds: this.associationIds };
+        return super.prepareSsmEntry(inputs);
+    }
+
+}
